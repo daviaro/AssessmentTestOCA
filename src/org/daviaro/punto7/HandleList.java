eb83f9f2eb353fa9f3289602946a4b19c27151b9
Adalbert David Aroca Lancheros <daviaro@gmail.com>
2017-10-30 23:01:23 -0500

    Ajuste sobre la clase del punto 7 del manejo de las listas

--- src/org/daviaro/punto7/HandleList.java
+++ src/org/daviaro/punto7/HandleList.java
@@ -17,6 +17,7 @@ public class HandleList {
     public static void main(String[] args) {
         int[] array = {6, 9, 8};
         List<Integer> list = new ArrayList<>();
+
         list.add(array[0]);
         list.add(array[2]);
         list.set(1, array[1]);
